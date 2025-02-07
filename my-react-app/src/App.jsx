19bae9843b97e5c11e2f3a9e22aa54039abe3c6b
Daniil <dangarby@gmail.com>
2025-02-07 16:56:27 +0200

    Initial React project with Card component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,24 +1,31 @@
+import Card from "./Card"; 
 import "./App.css";
 
 function App() {
   const data = [
     {
       img: "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg",
-      title: "Cute Dog",
-      description: "A lovely dog sitting on the grass.",
+      title: "Cute puppies",
+      description: "Two puppies sitting on the grass.",
     },
     {
       img: "https://images.pexels.com/photos/4587995/pexels-photo-4587995.jpeg",
-      title: "Happy Puppy",
-      description: "A small puppy enjoying the sunshine.",
+      title: "Sad dog",
+      description: "Sad dog.",
     },
   ];
 
   return (
     <div className="app">
-      {data.map((item, index) => (
-        <Card key={index} img={item.img} title={item.title} description={item.description} />
-      ))}
+    
+      <h3>Proj React Homework</h3>
+      
+    
+      <div className="card-container">
+        {data.map((item, index) => (
+          <Card key={index} img={item.img} title={item.title} description={item.description} />
+        ))}
+      </div>
     </div>
   );
 }
--- my-react-app/src/card.jsx
+++ my-react-app/src/card.jsx
@@ -1,11 +1,11 @@
 function Card({ img, title, description }) {
-    return (
-      <div className="card">
-        <img src={img} alt={title} />
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    );
-  }
-  
-  export default Card;
+  return (
+    <div className="card">
+      <img className="card-image" src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default Card;
